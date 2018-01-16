bfc58cfdab187a72539f4afed0bf175c9ecd6bd6
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-15 19:55:36 -0800

    lab version v0.9.1

--- lab.rb
+++ lab.rb
@@ -1,9 +1,9 @@
 class Lab < Formula
   desc "CLI tool for GitLab, like hub"
   homepage "https://github.com/zaquestion/lab"
-  url "https://github.com/zaquestion/lab/releases/download/v0.9.0/lab_0.9.0_darwin_amd64.tar.gz"
-  version "0.9.0"
-  sha256 "ef649de06b32f079ae64c346533f09e93d8869b21023074bb8933afd3b10ea0c"
+  url "https://github.com/zaquestion/lab/releases/download/v0.9.1/lab_0.9.1_darwin_amd64.tar.gz"
+  version "0.9.1"
+  sha256 "f06ee02fa4195fb39b0a1ed254ec568d48981c72ae3fffb6046225db55735273"
 
   def install
     bin.install "lab"
