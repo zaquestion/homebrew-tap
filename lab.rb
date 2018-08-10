74c9a9f9b989eb7c32831d2742a1dbae289fd0ee
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-10 01:19:53 -0700

    Brew formula update for lab version v0.13.0

--- lab.rb
+++ lab.rb
@@ -1,9 +1,9 @@
 class Lab < Formula
-  desc "CLI tool for GitLab, like hub"
+  desc "Lab wraps Git or Hub, making it simple to clone, fork, and interact with repositories on GitLab"
   homepage "https://github.com/zaquestion/lab"
-  url "https://github.com/zaquestion/lab/releases/download/v0.12.0/lab_0.12.0_darwin_amd64.tar.gz"
-  version "0.12.0"
-  sha256 "6131389ab36749f0cceb1e06f186e4c0e9b8a6532f171256aa327850bb108e15"
+  url "https://github.com/zaquestion/lab/releases/download/v0.13.0/lab_0.13.0_darwin_amd64.tar.gz"
+  version "0.13.0"
+  sha256 "ad2729e08e1ee07ab6a5f5cb6d1c08d844570f63fff0a8f3cf12b4c0c09356de"
 
   def install
     bin.install "lab"
